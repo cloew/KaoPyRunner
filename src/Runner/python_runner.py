98f3304b689137c321972ca1607c488b3a331f74
Chris Loew <cloew123@gmail.com>
2014-03-15 00:15:09 -0600

    Some refactoring of complicated code into helper functions

--- src/Runner/python_runner.py
+++ src/Runner/python_runner.py
@@ -17,17 +17,27 @@ class PythonRunner:
         
     def runFunction(self):
         """ Run the function """
-        newLines = list(self.functionLines)
-        for i in range(len(self.functionLines)):
-            if i+1 < len(self.functionLines):
-                nextLine = self.functionLines[i+1]
-                leadingWhitespace = nextLine[:len(nextLine)-len(nextLine.lstrip())]
-            else:
-                leadingWhitespace = "\t"
-            newLines[i+i+1:i+i+1] = ["{0}self.lineNumber = {1}".format(leadingWhitespace, i+1)]
-        newLines[0] = "def {0}(self):".format(self.functionName)
+        newLines = self.getNewFunctionLines()
         
         wholeFunction = "\n".join(newLines)
         exec(wholeFunction)
         exec("returnValue = {0}(self)".format(self.functionName))
         return self.lineNumber, returnValue
+        
+    def getNewFunctionLines(self):
+        """ Return the new function lines """
+        newLines = list(self.functionLines)
+        for i in range(len(self.functionLines)):
+            leadingWhitespace = self.getLeadingWhitespaceForLine(i+1)
+            newLines[i+i+1:i+i+1] = ["{0}self.lineNumber = {1}".format(leadingWhitespace, i+1)]
+        newLines[0] = "def {0}(self):".format(self.functionName)
+        return newLines
+        
+    def getLeadingWhitespaceForLine(self, index):
+        """ Return the leading whitespace for the given line index """
+        if index < len(self.functionLines):
+            nextLine = self.functionLines[index]
+            leadingWhitespace = nextLine[:len(nextLine)-len(nextLine.lstrip())]
+        else:
+            leadingWhitespace = "\t"
+        return leadingWhitespace

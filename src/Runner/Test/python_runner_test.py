ec288567489a1786130e4773f2b306a3b2ed2f73
Chris Loew <cloew123@gmail.com>
2014-03-15 00:05:28 -0600

    Now track line numbers, but very crudely

--- src/Runner/Test/python_runner_test.py
+++ src/Runner/Test/python_runner_test.py
@@ -6,6 +6,7 @@ EMPTY_METHOD = ["def testMethod():", "\tpass"]
 RETURN_1_METHOD = ["def testMethod():", "\treturn 1"]
 RETURN_STRING_METHOD = ["def testMethod():", "\treturn 'Blah'"]
 MULTI_LINE_RETURN_METHOD = ["def testMethod():", "\t", "\t", "\treturn 1"]
+EARLY_RETURN_METHOD = ["def testMethod():", "\tif True:", "\t\treturn 2", "\t", "\treturn None"]
 
 class processFunction(unittest.TestCase):
     """ Test cases of processFunction """
@@ -14,8 +15,9 @@ class processFunction(unittest.TestCase):
         """ Test that the default return value is added properly """
         self.runner = PythonRunner(EMPTY_METHOD)
         results = self.runner.processFunction()
-        self.assertEquals("return None", results[1], "Should have the proper return statement")
-        self.assertEquals(1, len(results), "Should only have the return statement")
+        
+        expectedLine = len(EMPTY_METHOD)
+        self.assertEquals("return None", results[expectedLine], "Should have the proper return statement")
         
     def handlesReturnValue_Explicit(self):
         """ Test that an explicit return value is added properly """
@@ -25,22 +27,30 @@ class processFunction(unittest.TestCase):
         self.assertEquals(1, len(results), "Should only have the return statement")
         
     def handlesReturnValue_String(self):
-        """ Test that an explicit return value is added properly """
+        """ Test that an explicit string return value is added properly """
         self.runner = PythonRunner(RETURN_STRING_METHOD)
         results = self.runner.processFunction()
         self.assertEquals("return 'Blah'", results[1], "Should have the proper return statement")
         self.assertEquals(1, len(results), "Should only have the return statement")
         
     def handlesReturnValue_MultiLine(self):
-        """ Test that an explicit return value is added properly """
+        """ Test that a multi line function's return value is added properly """
         self.runner = PythonRunner(MULTI_LINE_RETURN_METHOD)
         results = self.runner.processFunction()
         
         expectedLine = len(MULTI_LINE_RETURN_METHOD)-1
         self.assertEquals("return 1", results[expectedLine], "Should have the proper return statement")
+        
+    def handlesReturnValue_EarlyReturn(self):
+        """ Test that an early return value is added properly """
+        self.runner = PythonRunner(EARLY_RETURN_METHOD)
+        results = self.runner.processFunction()
+        
+        expectedLine = 2
+        self.assertEquals("return 2", results[expectedLine], "Should have the proper return statement")
 
 # Collect all test cases in this class
-testcasesProcessFunction = ["handlesReturnValue_Default", "handlesReturnValue_Explicit", "handlesReturnValue_String", "handlesReturnValue_MultiLine"]
+testcasesProcessFunction = ["handlesReturnValue_Default", "handlesReturnValue_Explicit", "handlesReturnValue_String", "handlesReturnValue_MultiLine", "handlesReturnValue_EarlyReturn"]
 suiteProcessFunction = unittest.TestSuite(map(processFunction, testcasesProcessFunction))
 
 ##########################################################
--- src/Runner/python_runner.py
+++ src/Runner/python_runner.py
@@ -1,4 +1,6 @@
 
+lineNumber = 0
+
 class PythonRunner:
     """ Represents a runner of a Python class """
     
@@ -10,14 +12,24 @@ class PythonRunner:
         
     def processFunction(self):
         """ Processes the given function """
-        returnValue = self.runFunction()
+        lineNumber, returnValue = self.runFunction()
         if type(returnValue) == str:
             returnValue = "'{0}'".format(returnValue)
-        return {len(self.functionLines)-1:"return {0}".format(returnValue)}
+        return {lineNumber:"return {0}".format(returnValue)}
         
     def runFunction(self):
         """ Run the function """
-        wholeFunction = "\n".join(self.functionLines)
+        newLines = list(self.functionLines)
+        for i in range(len(self.functionLines)):
+            if i+1 < len(self.functionLines):
+                nextLine = self.functionLines[i+1]
+                leadingWhitespace = nextLine[:len(nextLine)-len(nextLine.lstrip())]
+            else:
+                leadingWhitespace = "\t"
+            newLines[i+i+1:i+i+1] = ["{0}lineNumber = {1}".format(leadingWhitespace, i+1)]
+        newLines[1:1] = ["\tglobal lineNumber"]
+        
+        wholeFunction = "\n".join(newLines)
         exec(wholeFunction)
         exec("returnValue = {0}()".format(self.functionName))
-        return returnValue
+        return lineNumber, returnValue

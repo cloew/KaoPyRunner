dd9924b62649f839a180dadc31e0e12b9e85924a
Chris Loew <cloew123@gmail.com>
2014-03-15 12:42:20 -0600

    Now store the state additions for each line

--- src/Runner/python_runner.py
+++ src/Runner/python_runner.py
@@ -7,17 +7,22 @@ class PythonRunner:
         self.functionHeader = functionLines[0]
         self.functionName = self.functionHeader.replace('def', '').strip().split('(')[0]
         self.functionLines = functionLines
+        self.previousState = None
+        self.functionStates = {}
+        self.lineNumber = 0
         
     def processFunction(self):
         """ Processes the given function """
-        lineNumber, returnValue = self.runFunction()
+        lastLineNumber, returnValue = self.runFunction()
         if type(returnValue) == str:
             returnValue = "'{0}'".format(returnValue)
             
         results = {}
-        for varName in self.variables:
-            results[1] = "{0} = {1}".format(varName, self.variables[varName])
-        results[lineNumber] = "return {0}".format(returnValue)
+        for lineNumber in self.functionStates:
+            functionState = self.functionStates[lineNumber]
+            for varName in functionState:
+                results[lineNumber] = "{0} = {1}".format(varName, functionState[varName])
+        results[lastLineNumber] = "return {0}".format(returnValue)
         return results
         
     def runFunction(self):
@@ -34,12 +39,12 @@ class PythonRunner:
         newLines = list(self.functionLines)
         for i in range(len(self.functionLines)):
             leadingWhitespace = self.getLeadingWhitespaceForLine(i+1)
-            housekeepingCommands = ["{0}__runner__.lineNumber = {1}".format(leadingWhitespace, i+1),
-                                    leadingWhitespace+"__runner__.variables = {}", 
-                                    leadingWhitespace+"for __var_name__ in [__var_name__ for __var_name__ in dir() if __var_name__ != 'self' and __var_name__ != '__var_name__']:",
-                                    leadingWhitespace+"\t__runner__.variables[__var_name__]=eval(__var_name__)"]
+            housekeepingCommands = ["__variables__ = {}", 
+                                    "for __var_name__ in [__var_name__ for __var_name__ in dir() if __var_name__ not in ['__runner__', '__var_name__', '__variables__']]:",
+                                    "\t__variables__[__var_name__]=eval(__var_name__)",
+                                    "__runner__.storeState({0}, __variables__)".format(i+1)]
             newIndex = i+1+len(housekeepingCommands)*i
-            newLines[newIndex:newIndex] = housekeepingCommands
+            newLines[newIndex:newIndex] = [leadingWhitespace+command for command in housekeepingCommands]
         newLines[0] = "def {0}(__runner__):".format(self.functionName)
         return newLines
         
@@ -51,3 +56,13 @@ class PythonRunner:
         else:
             leadingWhitespace = "\t"
         return leadingWhitespace
+        
+    def storeState(self, lineNumber, variables):
+        """ Store the current state of the function """
+        self.functionStates[self.lineNumber] = {}
+        for varName in variables:
+            if self.previousState is None or varName not in self.previousState:
+                self.functionStates[self.lineNumber][varName] = variables[varName]
+        self.lineNumber = lineNumber
+                
+        self.previousState = variables

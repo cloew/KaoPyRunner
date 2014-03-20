c57b2072e5b81735691f12b10d8174fb161a994f
Chris Loew <cloew123@gmail.com>
2014-03-19 22:00:59 -0600

    Added tests for other cases of parsing the header

--- src/Runner/Test/python_function_test.py
+++ src/Runner/Test/python_function_test.py
@@ -13,9 +13,19 @@ class parseHeader(unittest.TestCase):
         """ Test that no Arguments are handled properly """
         self.function.parseHeader("def testMethod():")
         self.assertEquals([], self.function.arguments, 'Arguments should be empty when there are no arguments')
+        
+    def singleArguments(self):
+        """ Test that a Single Argument is handled properly """
+        self.function.parseHeader("def testMethod(i):")
+        self.assertEquals(['i'], self.function.arguments, 'Arguments should be the argument provided')
+        
+    def multipleArguments(self):
+        """ Test that Multiple Arguments are handled properly """
+        self.function.parseHeader("def testMethod(i, j):")
+        self.assertEquals(['i', 'j'], self.function.arguments, 'Arguments should be the provided arguments')
 
 # Collect all test cases in this class
-testcasesParseHeader = ["noArguments"]
+testcasesParseHeader = ["noArguments", "singleArguments", "multipleArguments"]
 suiteParseHeader = unittest.TestSuite(map(parseHeader, testcasesParseHeader))
 
 ##########################################################
